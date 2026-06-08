7d2d4225a242e0ac550c2979e281f064651b510c
Denji-kun <128576451+Denji-kun@users.noreply.github.com>
2026-06-08 11:33:14 +0530

    Day 1: comments and variables

---
+++ Comments & Variables.py
@@ -0,0 +1,33 @@
+#print("hello world") --- printing hello world.
+#how are you are u fine?
+# i'm fine -------------- single line comments.
+
+#Comments 
+
+"""this is a example of 
+multi line --------------- multi line comment.
+comment using 3 double qoutes""" 
+
+
+#Variables
+ 
+#a = 26
+#b = 10
+
+#  a = 15 --- no space allowed anywhere in variables other then after variables name 
+# 1a= 14 ---- cant start variable using numbers allowed in between or last
+"""ab1= 14 
+print (ab1)"""
+
+
+""" nice = 15 ----- cant use special characters other than _
+print(okay-fine)""" 
+
+#camel case 
+#camelCase = 13 ---- first small then capital for understanding
+
+#pascal case
+#PascalCase = 9 --- all caps from starting word for understanding 
+
+#snake case
+#snake_case = 15 ---- for understanding, specifically for python, use "_"
